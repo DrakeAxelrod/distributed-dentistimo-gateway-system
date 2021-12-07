5d821ad7a0c035f7b06c5a46f1e9e82437b2feef
Robin Hansen <gushanrod@student.gu.se>
2021-12-07 23:21:35 +0100

    someGatewayChanges
    Co-authored-by: axelrod <axelrod@student.chalmers.se>
    Co-authored-by: asiya <asiymo@student.chalmers.se>
    Co-authored-by: simon <simoar@student.chalmers.se>

---
+++ src/bookings/index.js
@@ -0,0 +1,13 @@
+const client = require("../Client")
+const { log } =  console
+
+const responsePath = "frontend/bookings"
+
+module.exports = bookingsHandler = (t, m) => {
+    client.emit(t, t, m)
+};
+
+client.on('all', (t, m) => {
+    console.log(m)
+    client.publish(`${responsePath}/${t}`, m)
+})
